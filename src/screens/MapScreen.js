70a6cd5046da8b5a420ceca233df1f47d6712fdc
gary <ggrumbley@ucsd.edu>
2017-07-30 19:33:19 -0700

    Add jobs reducer

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -26,7 +26,9 @@ class MapScreen extends Component {
   }
 
   _onButtonPress = () => {
-    this.props.fetchJobs(this.state.region);
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck');
+    });
   }
 
   render () {
