432dc7d7b57e02fadb22c1c41caf4fe648630768
gary <ggrumbley@ucsd.edu>
2017-08-07 13:08:22 -0700

    Add like job feature

--- src/screens/DeckScreen.js
+++ src/screens/DeckScreen.js
@@ -1,17 +1,78 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
+
+import * as actions from '../actions';
+import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+
+  _renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.045,
+      latitudeDelta: 0.02
+    };
+
+    return (
+      <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={initialRegion}
+          >
+          </MapView>
+        </View>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+        </Text>
+      </Card>
+    );
+  }
+
+  _renderNoMoreCards() {
+    return (
+      <Card title="No More Jobs">
+
+      </Card>
+    )
+  }
+
   render () {
     return (
-      <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+      <View style={{ marginTop: 10 }}>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this._renderCard}
+          renderNoMoreCards={this._renderNoMoreCards}
+          onSwipeRight={(job) => this.props.likeJob(job)}
+          keyProp="jobkey"
+        />
       </View>
     )
   }
 }
 
-export default DeckScreen;
+const styles = StyleSheet.create({
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+});
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    jobs: state.jobs.results
+  };
+}
+export default connect(mapStateToProps, actions)(DeckScreen);
