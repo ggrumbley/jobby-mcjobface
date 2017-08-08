edc04d72526ec6f3ac7091f85fc8ad778e03df24
gary <ggrumbley@ucsd.edu>
2017-08-08 13:38:00 -0700

    Add push notifications

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Notifications } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 
 import store from './src/store';
+import registerForNotifications from './src/services/push_notifications';
 import AuthScreen from './src/screens/AuthScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import MapScreen from './src/screens/MapScreen';
@@ -12,6 +14,21 @@ import SettingsScreen from './src/screens/SettingsScreen';
 import ReviewScreen from './src/screens/ReviewScreen';
 
 class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+
+      if (origin === 'received' && text) {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok.' }]
+        );
+      }
+    });
+  }
+
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
--- src/reducers/likes_reducer.js
+++ src/reducers/likes_reducer.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
 import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
+import { REHYDRATE } from 'redux-persist/constants';
 
 export default (state=[], action) => {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likedJobs || [];
     case CLEAR_LIKED_JOBS:
       return [];
     case LIKE_JOB:
